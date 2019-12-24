097a960112cb3aaffcfcc7a05b881603624ad04b
khimin1719 <khimin1719@gmail.com>
2019-12-24 20:59:53 +0200

    added web drivers versions

--- src/main/java/com/project/inftrastructure/utils/property/ApplicationPropNames.java
+++ src/main/java/com/project/inftrastructure/utils/property/ApplicationPropNames.java
@@ -1,15 +1,7 @@
 package com.project.inftrastructure.utils.property;
 
 public enum ApplicationPropNames {
-    BROWSER("BROWSER"),
-    IMPLICITLY_WAIT_TIMEOUT("IMPLICITLY_WAIT_TIMEOUT"),
-    HUB_URL("HUB_URL"),
-
-//  Drivers paths
-    CHROME_DRIVER_PATH("CHROME_DRIVER_PATH"),
-    INTERNET_EXPLORER32_PATH("INTERNET_EXPLORER32_PATH"),
-    FIREFOX_DRIVER_PATH("FIREFOX_DRIVER_PATH");
-
+    IMPLICITLY_WAIT("project.implicitWait");
     private String prop;
 
     ApplicationPropNames(String prop) {
