6730ffaeebbc440f6435fc4fdf8fec230c2314b1
khimin1719 <khimin1719@gmail.com>
2019-12-28 11:09:25 +0200

    fix

--- wait-for-grid.sh
+++ wait-for-grid.sh
@@ -6,7 +6,7 @@ set -e
 cmd="$@"
 
 echo 'Check Selenium Grid'
-while ! curl -sSL "http://selenium-hub:4444/wd/hub/status" 2>&1 \
+while ! curl -sSL "http://hub:4444/wd/hub/status" 2>&1 \
         | jq -r '.value.ready' 2>&1 | grep "true" >/dev/null; do
     echo 'Waiting for the Grid'
     sleep 1
