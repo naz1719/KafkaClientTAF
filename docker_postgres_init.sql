83a33ec5468599385edafff797ada92970ed20d5
nazarii.khimin <khimin1719@gmail.com>
2021-06-22 16:50:00 +0300

    added init db table creation

--- docker_postgres_init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE EMPLOYEE
-(
-    ID serial,
-    MESSAGE varchar(100) NOT NULL,
-    PRIMARY KEY (ID)
-);
---
+++ sql/create_tables.sql
@@ -0,0 +1,6 @@
+-- Creation of product table
+CREATE TABLE IF NOT EXISTS EMPLOYEE1 (
+  ID INT NOT NULL,
+  MESSAGE varchar(250) NOT NULL,
+  PRIMARY KEY (ID)
+);
