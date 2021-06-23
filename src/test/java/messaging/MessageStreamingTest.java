037b3c15bedfab6443452ee4ead12b85651e20db
khimin1719 <khimin1719@gmail.com>
2021-06-23 10:54:58 +0300

    added cast from LinkedHashMap to Object

--- src/test/java/messaging/MessageStreamingTest.java
+++ src/test/java/messaging/MessageStreamingTest.java
@@ -1,23 +1,27 @@
 package messaging;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.infrastructure.middlewares.kafka.ConsumerService;
+import com.project.infrastructure.middlewares.kafka.ProducerService;
+import com.project.messages.dto.MessageDTO;
 import com.project.messages.model.Message;
 import com.project.services.kafka.check.MessageCheck;
 import com.project.services.kafka.repository.MessageRepository;
-import com.project.messages.dto.MessageDTO;
-import com.project.infrastructure.middlewares.kafka.ConsumerService;
-import com.project.infrastructure.middlewares.kafka.ProducerService;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MessageStreamingTest {
 
     ProducerService producerService = new ProducerService();
     ConsumerService consumerService = new ConsumerService();
     MessageRepository messageRepository = new MessageRepository();
+    ObjectMapper mapper = new ObjectMapper();
 
     @Test(description = "Produce/Consume event test, positive scenario")
     public void positiveMessageStreamingTest() {
@@ -47,9 +51,11 @@ public class MessageStreamingTest {
         producerService.produceEvents(inputTopicName, messageList);
         messageRepository.insertMessageList(messageList);
         List<ConsumerRecord<String, Message>> consumerRecordList = consumerService.consumeMessages(10, inputTopicName);
-        return consumerRecordList.stream()
-                .map(ConsumerRecord::value)
+        List<Message> consumedMessageList = consumerRecordList.stream()
+                .map(stringMessageConsumerRecord ->
+                        mapper.convertValue(stringMessageConsumerRecord.value(), new TypeReference<Message>() {}))
                 .collect(Collectors.toList());
+        return consumedMessageList;
     }
 
     private List<MessageDTO> prepareExpectedMessageDTOList(MessageRepository messageRepository,
